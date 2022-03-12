1543d5bf6fea76fa0fdb23262491693ef11a90a7
Oh Seung Hyeok <brown8167@gmail.com>
2022-03-12 21:41:25 +0900

    Create project

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+// ESM : import export 
+// CommonJS require() module.exports
+
+// import autoprefixer from 'autoprefixer'
+//
+// export {
+//     Plugins: [
+//         autoprefixer
+//     ]
+// }
+
+
+
+// const autoprefixer = require('autoprefixer')
+//
+// module.exports = {
+//     Plugins: [
+//         autoprefixer
+//     ]
+// }
+module.exports = {
+    plugins: [
+      require("autoprefixer"),
+    ],
+};
